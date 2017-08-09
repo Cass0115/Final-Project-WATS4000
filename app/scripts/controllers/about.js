d39c993119f1d513167e6a39fd785da08e70662f
Cassie Durr <Cass0115@gmail.com>
2017-08-08 18:47:25 -0700

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finalProjectWats4000App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the finalProjectWats4000App
+ */
+angular.module('finalProjectWats4000App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finalProjectWats4000App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the finalProjectWats4000App
+ */
+angular.module('finalProjectWats4000App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
