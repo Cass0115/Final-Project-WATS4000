15f495dcb94acb893b5acf2a4094a584d9b4e5d4
Cassie Durr <Cass0115@gmail.com>
2017-08-12 13:25:13 -0700

    update error

--- app/scripts/services/flickr.js
+++ app/scripts/services/flickr.js
@@ -8,18 +8,18 @@
  * Factory in the finalProjectWats4000App.
  */
 //angular.module('finalProjectWats4000App')
-  .factory('flickr', function ($resource) {
+//  .factory('flickr', function ($resource) {
     // Service logic
     // ...
 
     // Public API here
-    return $resource ('https://api.flickr.com/services/feeds/photos_public.gne?APPID=b523d26cf6c2a4fbb1e109f3708f8628')
-    query: {
-      method:'GET',
-      params:{
-        citySearch: 'Seattle'
-      },
-      isArray:false
-    }
-  });
-});
+//    return $resource ('https://api.flickr.com/services/feeds/photos_public.gne?APPID=b523d26cf6c2a4fbb1e109f3708f8628')
+//    query: {
+//      method:'GET',
+//      params:{
+//        citySearch: 'Seattle'
+//      },
+//      isArray:false
+//    }
+//  });
+//});
